e5aa5c754a35d980140e499aa829d7a73a9789dd
Eloy Espinaco <eloyesp@gmail.com>
2012-03-27 16:44:12 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diputados::Application.config.secret_token = 'c1022360bd21cc3287a0527b7072b157d3b5fb358e82a29dbc0ae35e449b6e03a783c05bd92e62249c5a62a47a1b536153b691e579b370a03085e4f76dfe05c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Diputados::Application.config.session_store :cookie_store, key: '_diputados_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Diputados::Application.config.session_store :active_record_store
