bb200b03e11494bbf32b67770df2bc4292fdd3fe
Olvap <oldani.pablo@gmail.com>
2012-03-28 09:11:09 -0300

    FIX: arrelgo un poco expedientes.rb para que no ocupe tanto espacio

--- app/admin/expedientes.rb
+++ app/admin/expedientes.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Expediente do
-  
+
+  filter :numero
+  filter :tipo
+  filter :letra
+  filter :fechaentr
+  filter :autor
+  filter :firmantes
+  filter :descripcion
+
+  index do
+    column :numero
+    column :tipo
+    column :letra
+    column :pasada
+    column :fechaentr
+  end
+
 end
