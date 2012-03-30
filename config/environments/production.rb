60aa677845d4527e5d834a9d21fc7a05e183e75f
Eloy Espinaco <eloyesp@gmail.com>
2012-03-30 17:28:36 -0300

    assets for production!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Diputados::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -46,7 +46,7 @@ Diputados::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( active_admin.js active_admin.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -65,3 +65,4 @@ Diputados::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
+
