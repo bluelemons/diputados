c13940e300de8c55dfe57f94a3e6dc7cd3eb26f2
Eloy Espinaco <eloyesp@gmail.com>
2012-04-15 17:40:46 -0300

    FIX: migraciones aplicadas.

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -1,5 +1,6 @@
 # Encoding: utf-8
 require 'legacy/migration'
+require 'legacy/tables'
 
 module Legacy
   # El encoding utilizado en las tablas dBase de FoxPro
@@ -10,7 +11,23 @@ module Legacy
   # Realiza la migración de todas las tablas e indica los resultados en la
   # consola
   def self.seed_legacy_databases
-    # TODO
+    LEGACY_TABLES.each do |tabla|
+      begin
+        puts "Migrando: #{tabla[:legacy]}\n"
+        migracion = Migration.new(tabla)
+
+        if migracion.legacy_table.record_count == migracion.model.count
+          raise "Tabla ya migrada y sin registros nuevos #{migracion.legacy_table.record_count} #{migracion.model.count}"
+        else
+          migracion.model.delete_all
+        end
+
+        migracion.run
+        puts "\n---\n#{tabla[:legacy]} migrado correctamente"
+      rescue
+        puts "\n---\n#{tabla[:legacy]} #{$!}"
+      end
+    end
   end
 end
 
