8cbb6bcaba0b079a4e224e3d1f4a053da98fa161
Eloy Espinaco <eloyesp@gmail.com>
2012-04-15 19:47:21 -0300

    TODO

--- app/models/estado.rb
+++ app/models/estado.rb
@@ -3,6 +3,9 @@ class Estado < ActiveRecord::Base
   belongs_to :expediente
 
   def self.seed(attr)
+    # REFACTOR:
+    # attr[:expediente_id] = Expediente.find_by_legacy_id(attr).id
+
     attr[:expediente_id] = Expediente.where("numero = ? and letra = ? and pasada = ? and tipo = ? ",
         attr[:numero], attr[:letra], attr[:pasada], attr[:tipo]).first.id
     super(:expediente_id, :fechaent, :comision, attr)
