d43db9875c42718253f0fce3f3cbc4ba0f3e1c57
Eloy Espinaco <eloyesp@gmail.com>
2012-04-23 17:29:15 -0300

    Corrijo el orden de las migraciones.
    Las tablas con belongs_to deben estar después de aquello a lo que referencian.

--- lib/legacy/tables.rb
+++ lib/legacy/tables.rb
@@ -7,16 +7,16 @@ module Legacy
   # Listado de las tablas a migrar en el orden que debe ser realizado.
 
   LEGACY_TABLES = [
+    {:model=> Tema, :legacy=>"temas.dbf"},
+    {:model=> Status, :legacy=>"status.dbf"},
     {:model=> Expediente, :legacy=>"proyecto.dbf"},
     {:model=> Final, :legacy=>"final.dbf"},
-    {:model=> Senado, :legacy=>"senado.dbf"},
     {:model=> Asunto, :legacy=>"asunto.dbf"},
     {:model=> Estado, :legacy=>"estado.dbf"},
     {:model=> Sesion, :legacy=>"sesion.dbf"},
-    {:model=> Tema, :legacy=>"temas.dbf"},
+    {:model=> Senado, :legacy=>"senado.dbf"},
     {:model=> Diputado, :legacy=>"diputado.dbf"},
-    {:model=> Prefer, :legacy=>"prefer.dbf"},
-    {:model=> Status, :legacy=>"status.dbf"}
+    {:model=> Prefer, :legacy=>"prefer.dbf"}
   ]
 end
 
