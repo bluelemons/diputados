ddc003a8dd241326e1615baea148debed68797ff
Olvap <oldani.pablo@gmail.com>
2012-04-28 16:50:23 -0300

    ADD: relaciones de sesion con la tablas resultado, tratamiento y periodo.

--- app/models/sesion.rb
+++ app/models/sesion.rb
@@ -6,5 +6,29 @@ class Sesion < ActiveRecord::Base
 
   # @return [Expediente]
   belongs_to :expediente
-end
 
+  Resultado = {
+    1 => "Aprobado sin modificaciones",
+    2 => "Aprobado con modificaciones",
+    3 => "Vuelta a comision",
+    4 => "Rechazado",
+    5 => "Al archivo"
+    }
+  def resultado
+    Resultado[read_attribute(:resuvotac)] || "No indicado"
+  end
+
+  Tratamiento = { 1 => "Orden del dia", 2 => "Sobre tablas", 3 => "Preferencial"}
+  def tratamiento
+    Tratamiento[read_attribute(:tratamient)] || "No indicado"
+  end
+
+  Periodo = { 1 => "Ordinario", 2 => "De prorroga", 3 => "Extraordinario"}
+  def tipoperiod
+    Periodo[read_attribute(:tipoperiod)] || "No indicado"
+  end
+
+  def periodo
+    "#{tipoperiod} Numero: #{numperiodo}"
+  end
+end
