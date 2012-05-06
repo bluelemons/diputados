797ac77cca3dfc99936b123598143ea553d28d25
Eloy Espinaco <eloyesp@gmail.com>
2012-05-06 16:08:30 -0300

    ADD: Asunto#comisiones
    Agrego tests. :)

--- test/unit/asunto_test.rb
+++ test/unit/asunto_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AsuntoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Se indican las comisiones asignadas" do
+    asunto = asuntos(:puente)
+    assert asunto.comisiones.first.kind_of?(Comision), "No tiene asignada comisiones"
+    assert_equal 3, asunto.comisiones.count, "La cantidad de comisiones esta mal"
+  end
 end
+
