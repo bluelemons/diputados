73a1fff050961d80167581792ee0df8c9ff787dd
Pablo oldani <oldani.pablo@gmail.com>
2012-05-15 15:41:51 -0300

    FIX: mejoro el listado

--- app/reports/expedientes_report.rb
+++ app/reports/expedientes_report.rb
@@ -45,7 +45,38 @@ class ExpedientesReport < Prawnbot::Report
     body
 
     show_title "DETALLE DE EXPEDIENTE"
+
+    report_line([
+      "#{expediente.clave}",
+      "<b>Autor</b> #{expediente.autor}",
+      "<b>Tema</b> #{expediente.tema}",
+      "<b>Estado</b> #{expediente.estado}",
+      " ",
+      "<b>Descripcion</b>",
+      "#{expediente.descrip}",
+      " ",
+      "<b>Firmantes</b> #{expediente.firmantes}",
+      " ",
+      "Entrada: #{expediente.fechaentr}, Por: #{expediente.tipoentr} a las #{expediente.hora} en el periodo #{expediente.tipoperiod} N #{expediente.numperiodo}"
+      ])
+
     show_title "PASE POR COMISIONES"
+    expediente.estados.each do |estado|
+
+      dic = ""
+      estado.dictamenes.each do |dictamen|
+        dic += "<b>#{dictamen[:tipo]}</b> <b>fecha</b>: #{dictamen[:fecha]}<br>"
+        dic += "#{dictamen[:dictamen]}<br>"
+      end
+
+      report_line([
+        "#{estado.comision.name}",
+        "<b>Entrada:</b>#{estado.fechaent}  <b>Salida:</b>#{estado.fechasal}",
+        dic
+      ])
+
+    end
+
     show_title "TRATAMIENTO EN SESION"
 
     render
