90b4c2391cc04f1fb030228a8b930792f5201a5c
Eloy Espinaco <eloyesp@gmail.com>
2012-06-04 07:47:49 -0300

    ADD: configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "diputados"
+set :repository,  "https://github.com/BlueLemon/diputados.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/home/diputados"
+
+server "diputados", :app, :web, :db, :primary => true
+
+set :user, "diputados"
+
+set :use_sudo, false
+
+#role :web, "diputados"                          # Your HTTP server, Apache/etc
+#role :app, "diputados"                          # This may be the same as your `Web` server
+#role :db,  "diputados", :primary => true        # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -23,3 +31,4 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
