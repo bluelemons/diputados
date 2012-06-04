18e967395987258683f62da4a2839ff0746671c7
Eloy Espinaco <eloyesp@gmail.com>
2012-06-04 11:45:25 -0300

    Config: passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,9 @@ set :scm, :git
 
 set :deploy_to, "/home/diputados"
 
-server "diputados", :app, :web, :db, :primary => true
+role :web, "diputados"                          # Your HTTP server, Apache/etc
+role :app, "diputados"                          # This may be the same as your `Web` server
+role :db,  "diputados", :primary => true        # This is where Rails migrations will run
 
 set :user, "diputados"
 
@@ -29,9 +31,15 @@ end
 
 after "deploy:finalize_update", "deploy:db:symlink"
 
-#role :web, "diputados"                          # Your HTTP server, Apache/etc
-#role :app, "diputados"                          # This may be the same as your `Web` server
-#role :db,  "diputados", :primary => true        # This is where Rails migrations will run
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
 #role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
@@ -39,13 +47,4 @@ after "deploy:finalize_update", "deploy:db:symlink"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
 
