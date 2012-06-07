e3ff2ac2f8884a40ff42e89add6936d9909c2129
Pablo oldani <oldani.pablo@gmail.com>
2012-06-07 11:43:05 -0300

    Vemos 5 por página por default

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,7 +6,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "CAMARA DE DIPUTADOS"
-
+  config.default_per_page = 5
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
