9b83e209eddd6dee778750a3469327599b5f406f
Pablo oldani <oldani.pablo@gmail.com>
2012-07-03 10:29:42 -0300

    FIX: pongo los metodos para los listados en un arhicov separado.

--- app/admin/expedientes.rb
+++ app/admin/expedientes.rb
@@ -12,22 +12,9 @@ ActiveAdmin.register Expediente do
     def index
       super do |format|
         format.pdf {
-          @expedientes = Expediente.search(params[:q])
-          report = ODFReport::Report.new(Rails.root.join("app/reports/expedientes.odt")) do |r|
-            r.add_table("EXPEDIENTES", @expedientes, :header=>true) do |t|
-              t.add_column(:clave) { |item| item.clave.to_s }
-              t.add_column(:entrada) { |item| item.entrada.to_s }
-              t.add_column(:autor) { |item| item.autor.to_s }
-              t.add_column(:firmantes) { |item| item.firmantes.to_s }
-              t.add_column(:descripcion) { |item| item.descrip.to_s }
-
-            end
-          end
-
-          report_file_name = report.generate
-
-          send_file(report_file_name)
-          }
+          report = ExpedientesReport.new.detalle 
+          send_file(report)
+        }
       end
     end
   end
@@ -52,45 +39,8 @@ ActiveAdmin.register Expediente do
 
   member_action :print do
 
-    @expediente = Expediente.find(params[:id])
-
-    report = ODFReport::Report.new(Rails.root.join("app/reports/expediente.odt")) do |r|
-
-      r.add_field(:clave,         @expediente.clave.to_s)
-      r.add_field(:autor,         @expediente.autor.to_s)
-      r.add_field(:tema,          @expediente.tema.to_s)
-      r.add_field(:estado,        @expediente.estado.to_s)
-      r.add_field(:descripcion,   @expediente.descrip.mb_chars.capitalize)
-      r.add_field(:firmantes,     @expediente.firmantes.to_s)
-
-      r.add_field(:fechaentr,     @expediente.fechaentr.to_s)
-      r.add_field(:tipoentr,      @expediente.tipoentr.to_s)
-      r.add_field(:hora,          @expediente.hora.to_s)
-      r.add_field(:tipoperiod,    @expediente.tipoperiod.to_s)
-      r.add_field(:numperiodo,    @expediente.numperiodo.to_s)
-
-      r.add_field(:tratamiento,   @expediente.sesion.tratamiento.to_s)
-      r.add_field(:resultado,     @expediente.sesion.resultado.to_s)
-      r.add_field(:fechases,      @expediente.sesion.fechases.to_s)
-      r.add_field(:periodo,       @expediente.sesion.periodo.to_s)
-
-      r.add_section "COMISION", @expediente.estados do |s|
-        s.add_field(:nombre) { |estado| estado.comision.nombre }
-        s.add_field(:entrada) { |estado| estado.fechaent }
-        s.add_field(:salida) { |estado| estado.fechasal }
-        s.add_section("DICTAMEN", :dictamenes) do |ss|
-
-          ss.add_field(:tipo) { |n| n[:tipo].to_s }
-          ss.add_field(:fecha) { |n| n[:fecha].to_s }
-          ss.add_field(:dictamen) { |n| n[:dictamen].to_s }
-        end
-      end
-
-    end
-
-    report_file_name = report.generate
-
-    send_file(report_file_name)
+    report = ExpedientesReport.new.listado(params)
+    send_file(report)
 
   end
 
---
+++ app/reports/expedientes_report.rb
@@ -0,0 +1,63 @@
+class ExpedientesReport
+  def detalle
+
+    @expedientes = Expediente.search(params[:q])
+
+    report = ODFReport::Report.new(Rails.root.join("app/reports/expedientes.odt")) do |r|
+      r.add_table("EXPEDIENTES", @expedientes, :header=>true) do |t|
+        t.add_column(:clave) { |item| item.clave.to_s }
+        t.add_column(:entrada) { |item| item.entrada.to_s }
+        t.add_column(:autor) { |item| item.autor.to_s }
+        t.add_column(:firmantes) { |item| item.firmantes.to_s }
+        t.add_column(:descripcion) { |item| item.descrip.to_s }
+
+      end
+    end
+
+    report_file_name = report.generate
+
+  end
+
+  def listado(params)
+
+    @expediente = Expediente.find(params[:id])
+
+    report = ODFReport::Report.new(Rails.root.join("app/reports/expediente.odt")) do |r|
+
+      r.add_field(:clave,         @expediente.clave.to_s)
+      r.add_field(:autor,         @expediente.autor.to_s)
+      r.add_field(:tema,          @expediente.tema.to_s)
+      r.add_field(:estado,        @expediente.estado.to_s)
+      r.add_field(:descripcion,   @expediente.descrip.mb_chars.capitalize)
+      r.add_field(:firmantes,     @expediente.firmantes.to_s)
+
+      r.add_field(:fechaentr,     @expediente.fechaentr.to_s)
+      r.add_field(:tipoentr,      @expediente.tipoentr.to_s)
+      r.add_field(:hora,          @expediente.hora.to_s)
+      r.add_field(:tipoperiod,    @expediente.tipoperiod.to_s)
+      r.add_field(:numperiodo,    @expediente.numperiodo.to_s)
+
+      r.add_field(:tratamiento,   @expediente.sesion.try(:tratamiento).to_s)
+      r.add_field(:resultado,     @expediente.sesion.try(:resultado).to_s)
+      r.add_field(:fechases,      @expediente.sesion.try(:fechases).to_s)
+      r.add_field(:periodo,       @expediente.sesion.try(:periodo).to_s)
+
+      r.add_section "COMISION", @expediente.estados do |s|
+        s.add_field(:nombre) { |estado| estado.comision.nombre }
+        s.add_field(:entrada) { |estado| estado.fechaent }
+        s.add_field(:salida) { |estado| estado.fechasal }
+        s.add_section("DICTAMEN", :dictamenes) do |ss|
+
+          ss.add_field(:tipo) { |n| n[:tipo].to_s }
+          ss.add_field(:fecha) { |n| n[:fecha].to_s }
+          ss.add_field(:dictamen) { |n| n[:dictamen].to_s }
+        end
+      end
+
+    end
+
+    report_file_name = report.generate
+
+  end
+
+end
