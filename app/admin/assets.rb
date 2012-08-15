a1b29315d7917f690a2debd1856c977c4b7820eb
Pablo oldani <oldani.pablo@gmail.com>
2012-08-15 17:29:47 -0300

    FIX: redirecciono los asset
    Despues de crear o borrar se redirecciona al show de la relacion polimorifica.

--- app/admin/assets.rb
+++ app/admin/assets.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Asset do
-  belongs_to :expediente
 
   form :html => {:multipart => true} do |f|
     f.inputs "Details" do
@@ -15,9 +14,19 @@ ActiveAdmin.register Asset do
     load_and_authorize_resource
     skip_load_resource :only => :index
 
+    before_filter :only => :show do
+#      flash[:notice] = flash[:notice].dup if flash[:notice]
+      asset = Asset.find(params[:id])
+      resource_config = active_admin_config.namespace.resource_for(asset.adjuntable.class)
+      redirect_to send(resource_config.route_instance_path, asset.adjuntable)
+    end
+
     def destroy
       destroy! do |format|
-        format.html {redirect_to admin_expediente_path(@expediente)}
+        format.html {
+          resource_config = active_admin_config.namespace.resource_for(@asset.adjuntable.class)
+          redirect_to send(resource_config.route_instance_path, @asset.adjuntable)
+        }
       end
     end
   end
