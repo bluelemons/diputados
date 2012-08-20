13d16b5391fcc63189b333b0f477cf35646ef465
Pablo oldani <oldani.pablo@gmail.com>
2012-08-19 23:27:30 -0300

    FIX: los mensajes estaban alreves

--- test/unit/nota_tipo_test.rb
+++ test/unit/nota_tipo_test.rb
@@ -3,27 +3,26 @@ require 'test_helper'
 class NotaTipoTest < ActiveSupport::TestCase
    test "El nombre no puede estar en blanco" do
      nota_tipo = NotaTipo.new(:name => "algun lado")
-     assert !nota_tipo.save, "No se pueden guardar tipos de notas sin letra"
+     assert !nota_tipo.save, "No se pueden guardar tipos de notas sin nombre"
    end
 
    test "La letra no puede estar en blanco" do
      nota_tipo = NotaTipo.new(:letra => "a")
-     assert !nota_tipo.save, "No se pueden guardar tipos de notas sin nombre"
+     assert !nota_tipo.save, "No se pueden guardar tipos de notas sin letra"
    end
 
    test "El nombre tiene que ser unico" do
-     @nota_tipo = nota_tipos(:administrativas)
      nota_tipo = NotaTipo.new(:name => "algun lado", :letra => "AO")
      assert !nota_tipo.save, "La letra debe ser unica"
    end
 
    test "La letra tiene que ser unico" do
-     @nota_tipo = nota_tipos(:administrativas)
      nota_tipo = NotaTipo.new(:name => "Administrativas Oficiales", :letra => "Letra")
      assert !nota_tipo.save, "El nombre debe ser unico"
    end
 
    test "No se pueden borrar tipo de notas si hay notas que lo usan" do
+     pending "hay que hacerlo mas adelante cuando esten las relaciones"
    end
 
 end
