c79ec3c2793d509e8dcbcf8ef00a5cbfab8dd61c
Pablo oldani <oldani.pablo@gmail.com>
2012-08-27 14:09:07 -0300

    ADD: agrego test de notas

--- test/functional/admin_notas_controller_test.rb
+++ test/functional/admin_notas_controller_test.rb
@@ -21,11 +21,27 @@ class Admin::NotasControllerTest < ActionController::TestCase
   test "should show new" do
     get :new
     assert_response :success
+    pending "Deberia fijarse que el form pida un pase inicial."
   end
 
   test "should show edit" do
     get :edit, id: @nota
     assert_response :success
+  end
+
+  test "new form has expected elements" do
+    get :new
+    assert_select "form[id=new_nota][action=/admin/notas]" do
+      assert_select "#nota_fojas", :count => 1
+      assert_select "#nota_iniciador_id", :count => 1
+      assert_select "#nota_pases_attributes_0_area_id", :count => 1
+      assert_select "#nota_pases_attributes_0_ingreso", :count => 1
+      assert_select "#nota_pases_attributes_0_descripcion", :count => 1
+    end
+  end
+
+  test "show view must show a link to create a new Pase" do
+    pending
   end
 
 end
