638584d442f47117423e98a8e5f46d9e0495a713
Eloy Espinaco <eloyesp@gmail.com>
2012-08-26 23:31:48 -0300

    PIMP

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,9 +3,5 @@ class Asset < ActiveRecord::Base
 #  attr_accessible :asset
   has_attached_file :asset
 
-  include Rails.application.routes.url_helpers # neeeded for _path helpers to work in models
-
-  def admin_permalink
-    admin_expediente_asset_path(self.expediente,self)
-  end
 end
+
