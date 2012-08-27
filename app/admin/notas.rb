56eab08dca5e465212ce8ed1f75afa6d0812b5b8
Pablo oldani <oldani.pablo@gmail.com>
2012-08-27 14:07:15 -0300

    FIX: cambio el nombre del partial

--- app/admin/notas.rb
+++ app/admin/notas.rb
@@ -1,14 +1,18 @@
 ActiveAdmin.register Nota do
   menu :if => proc{ can?(:create, Nota) }
 
-  def new
-    @nota = Nota.new
-    @nota.pases.build
-    new!
+  controller do
+
+    def new
+      @nota = Nota.new
+      @nota.pases.build
+      new!
+    end
+
   end
 
   form do |f|
-    render :partial => 'nota'
+    render :partial => 'form'
   end
 
 end
