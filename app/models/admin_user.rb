5499c076dbef54b7c0d7c2c2f977784b27b9d582
Pablo oldani <oldani.pablo@gmail.com>
2012-09-03 00:27:38 -0300

    ADD: los usuarios trabajan en areas.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,18 +1,22 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
+         :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :role_ids,:roles_tokens
+  attr_accessible :email, :password, :password_confirmation,
+                  :remember_me, :role_ids, :roles_tokens, :area_ids
 
-  has_and_belongs_to_many :roles, :class_name => "Role",
-                          :join_table => :admin_users_roles, :uniq => true
+  has_and_belongs_to_many :roles, :uniq => true
 
   attr_reader :roles_tokens
+
+  has_and_belongs_to_many :areas, :uniq => true
 
   def roles_tokens=(ids)
     self.role_ids = ids.split(",")
   end
+
 end
+
