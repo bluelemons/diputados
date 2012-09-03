0ac82989df4191226010778d1fef1929e03c514d
Eloy Espinaco <eloyesp@gmail.com>
2012-09-03 11:39:06 -0300

    TEST: el test funcional sube un archivo

--- test/functional/assets_controller_test.rb
+++ test/functional/assets_controller_test.rb
@@ -4,20 +4,24 @@ class AssetsControllerTest < ActionController::TestCase
 
   setup do
     @asset = assets(:escaneo_puente)
+    @expediente = expedientes(:puente)
     stub_permisive_ability
   end
 
   test "should create asset" do
+    @asset = Asset.new
+    @asset.asset = fixture_file_upload('files/dc1378212.pdf')
+
     assert_difference('Asset.count') do
-      post :create, expediente_id: expedientes(:puente), asset: @asset.attributes
+      post :create, expediente_id: @expediente.to_param, asset: @asset
     end
 
-    assert_redirected_to [:admin, @asset.adjuntable]
+    assert_redirected_to [:admin, @expediente]
   end
 
   test "should destroy asset" do
     assert_difference('Asset.count', -1) do
-      delete :destroy, expediente_id: expedientes(:puente), id: @asset
+      delete :destroy, expediente_id: @expediente.to_param, id: @asset.to_param
     end
 
     assert_redirected_to [:admin, @asset.adjuntable]
