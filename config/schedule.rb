eedcbcec9d37546e5be3ed7648f74bc6fd937616
Eloy Espinaco <eloyesp@gmail.com>
2012-10-18 17:11:55 -0300

    corrijo las tareas de cron segun los dias - fix #40
    no se cómo testear esto

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,18 +7,31 @@
 
 set :output, "log/cron_log.log"
 
-every 1.hours do
-  command "/usr/bin/rsync --archive /datos/compartir/smbtramite/01-PROYECTOS/ /datos/local_pdf/"
-end
+      # .---------------- minute (0 - 59)
+      # |   .------------- hour (0 - 23)
+      # |   |   .---------- day of month (1 - 31)
+      # |   |   |   .------- month (1 - 12) OR jan,feb,mar,apr ...
+      # |   |   |   |  .----- day of week (0 - 7) (Sunday=0 or 7)  OR sun,mon,tue,wed,thu,fri,sat
+      # |   |   |   |  |
+      # *   *   *   *  *  command to be executed
 
-every 2.hours do
-#  command "/usr/bin/some_great_command"
-#  runner "MyModel.some_method"
+# Lunes, Martes y Viernes, cada 2 horas.
+every '30 5-23/2 * * mon,tue,fri' do
+  command "/usr/bin/rsync --archive /datos/compartir/smbtramite/01-PROYECTOS/ /datos/local_pdf/"
   rake "db:seed"
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+
+# Miercoles (cada hora).
+every '30 6-23 * * wed' do
+  command "/usr/bin/rsync --archive /datos/compartir/smbtramite/01-PROYECTOS/ /datos/local_pdf/"
+  rake "db:seed"
+end
+
+# los jueves cada media hora entre las 6:20 y las 23:50
+every '20-59/30 6-23 * * thu' do
+  command "/usr/bin/rsync --archive /datos/compartir/smbtramite/01-PROYECTOS/ /datos/local_pdf/"
+  rake "db:seed"
+end
 
 # Learn more: http://github.com/javan/whenever
+
