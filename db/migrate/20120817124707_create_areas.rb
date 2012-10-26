4e0edad5e660a0164737bfb11e5ace359e7f8bd5
Eloy Espinaco <eloyesp@gmail.com>
2012-10-26 15:14:50 -0300

    Corrijo migración segun master.
    Había un conflicto en la migración, tomo la versión de producción.

--- db/migrate/20120817124707_create_areas.rb
+++ db/migrate/20120817124707_create_areas.rb
@@ -1,10 +1,12 @@
 class CreateAreas < ActiveRecord::Migration
   def change
     create_table :areas do |t|
-      t.string :name
+      t.string  :name
       t.integer :area_id
+      t.string  :type
 
       t.timestamps
     end
   end
 end
+
