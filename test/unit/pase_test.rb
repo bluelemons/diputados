b575cc0e130ad78a47bb37b0758aa3c93828623b
Eloy Espinaco <eloyesp@gmail.com>
2012-10-29 09:00:19 -0300

    fixup! 3c1adc7a9f385f49db4326e036ab01e58fc2f34c

--- test/unit/pase_test.rb
+++ test/unit/pase_test.rb
@@ -2,23 +2,18 @@
 require 'test_helper'
 
 class PaseTest < ActiveSupport::TestCase
-  test "Datos de pase" do
-    @nota = expedientes(:despacho)
-    @area = areas(:comision)
-    pase = Pase.new
-    assert !pase.save, "falta fecha de ingreso"
-    pase.ingreso = Date.new
-    assert !pase.save, "falta un destino"
-    pase.area = @area
-    assert pase.save
-  end
 
-  test "Un pase solo se puede borrar si es el ultimo pase de una nota" do
-    @nota = expedientes(:despacho)
-    @pase = @nota.pases.first
-    assert !@pase.destroy, "no se pueden borrar pases a menos que sea el ultimo"
-    @pase = @nota.pases.last
-    assert @pase.destroy
+  test "Datos de pase" do
+    @pase = pases(:entrada)
+    assert @pase.valid?, "Fixture invalido: #{@pase.errors.full_messages}"
+#    @nota = expedientes(:despacho)
+#    @area = areas(:comision)
+#    pase = Pase.new
+#    assert !pase.save, "falta fecha de ingreso"
+#    pase.ingreso = Date.new
+#    assert !pase.save, "falta un destino"
+#    pase.area = @area
+#    assert pase.save
   end
 
   test "Un pase no puede tener una fecha de ingreso mayor a la del dia" do
@@ -62,3 +57,4 @@ class PaseTest < ActiveSupport::TestCase
   end
 
 end
+
