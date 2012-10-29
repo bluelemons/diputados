8e61b32becf9f201e528ec67a30da78dfc100a88
Eloy Espinaco <eloyesp@gmail.com>
2012-10-28 23:01:47 -0300

    ADD: Seeds de roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@ require 'legacy'
 
 Legacy.seed_legacy_databases
 
-AdminUser.find_or_create_by_email("admin@example.com", :password => "password")
+admin_role = Role.find_or_create_by_name('Admin', :observations => "Acceso completo al sistema.")
+Role.find_or_create_by_name('Mesa_de_Entrada', :observations => "Subir y borrar documentos escaneados.")
+
+AdminUser.find_or_create_by_email("admin@example.com", :password => "password", :roles => [admin_role])
 
