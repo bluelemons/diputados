c5aa809ee7249b4ef15ae39bc23f26e9bc1921c8
Pablo Oldani <oldani.pablo@gmail.com>
2013-01-25 20:14:18 -0300

    Fixed root Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Diputados::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'admin/expedientes#index'
+  root :to => 'admin/proyectos#index'
 
   # See how all your routes lay out with "rake routes"
 
