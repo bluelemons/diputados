a589d7c60836ed5600f5bfcd1868810379671f4d
Oldani Pablo <oldani.pablo@gmail.com>
2013-03-10 14:59:08 -0300

    Update asunto.rb
    El orden estaba exactamente alreves

--- app/models/asunto.rb
+++ app/models/asunto.rb
@@ -7,8 +7,8 @@ class Asunto < ActiveRecord::Base
 
   # @return [Array<Comision>] Comisiones asignadas en el orden correcto.
   def comisiones
-    comisiones_ids = [comision1, comision2, comision3, comision4, comision5,
-      comision6].delete_if { |i| i == 0 }
+    comisiones_ids = [comision6, comision5, comision4, comision3, comision2,
+      comision1].delete_if { |i| i == 0 }
     # I make use of an on memory array to avoid multiple queries.
     comisiones_array = Comision.where(:id => comisiones_ids).all
     comisiones_ids.map do |i|
