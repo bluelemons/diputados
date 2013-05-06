ef510112598e3c63b93d15b1feac5a104b644f02
Eloy Espinaco <eloyesp@gmail.com>
2013-05-05 21:42:31 -0300

    Fix specs on Notes.

--- spec/models/nota_spec.rb
+++ spec/models/nota_spec.rb
@@ -3,13 +3,24 @@ require 'spec_helper'
 describe Nota do
 
   context 'when creating a new expediente' do
+
+    it 'should create a new initiator and relate it to the expediente' do
+      nota = FactoryGirl.create :nota
+      kind = FactoryGirl.create :initiators_kind
+      Initiator.where(name: 'judicial').count.should == 0
+      nota.initiator_attributes = {name: 'judicial', initiators_kind_id: kind.id}
+      Initiator.where(name: 'judicial').count.should == 1
+    end
+
     it 'should relate it to an existent initiator' do
       nota = FactoryGirl.create :nota
       kind = FactoryGirl.create :initiators_kind
-      nota.initiator = {name: 'judicial', initiators_kind_id: kind.id}
+      Initiator.where(name: 'judicial').count.should == 0
+      nota.initiator_attributes = {name: 'judicial', initiators_kind_id: kind.id}
+      Initiator.where(name: 'judicial').count.should == 1
+      nota.initiator_attributes = {name: 'judicial', initiators_kind_id: kind.id}
+      Initiator.where(name: 'judicial').count.should == 1
     end
-      
-    it 'should create a new initiator and relate it to the expediente'
   end
-  
+
 end
