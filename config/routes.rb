db0667c1254d802ba979d6e126a44b7f196cb230
lmhsjackson <jackson@folder.(none)>
2013-06-10 11:27:06 -0300

    Autogenerated files from registering notas resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Diputados::Application.routes.draw do
 
+  namespace :backend do resources :notas end
+
   resources :expedientes, :only => [] do
     resources :assets, :only => [:create, :destroy]
   end
