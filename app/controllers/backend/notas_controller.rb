d6e821cb7ba22813726d03973140a0a4b5a53a30
lmhsjackson <josepiccioni@gmail.com>
2013-06-13 14:32:42 -0300

    nota new action redefined now it builds pases before rendering the form

--- app/controllers/backend/notas_controller.rb
+++ app/controllers/backend/notas_controller.rb
@@ -1,4 +1,10 @@
 class Backend::NotasController < Backend::ApplicationController
+
+def new
+  @nota = Nota.new
+  @nota.build_pases
+end
+
   def attributes
     [:id, :year, :ingreso, :area]
   end
