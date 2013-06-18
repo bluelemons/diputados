8a038cb53df4936c3502f4a3fa6a264f754f6c62
lmhsjackson <josepiccioni@gmail.com>
2013-06-18 11:30:59 -0300

    Indentation and whitespaces

--- app/controllers/backend/notas_controller.rb
+++ app/controllers/backend/notas_controller.rb
@@ -1,11 +1,12 @@
 class Backend::NotasController < Backend::ApplicationController
 
-def new
-  @nota = Nota.new
-  @nota.build_pases
-end
+  def new
+    @nota = Nota.new
+    @nota.build_pases
+  end
 
   def attributes
     [:id, :year, :ingreso, :area]
   end
+
 end
