553a69f50e4447201900be115bb5361849aa57f3
olvap <oldani.pablo@gmail.com>
2013-07-07 18:27:51 -0300

    Remove duplicate calls.

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -7,8 +7,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require jquery
-//= require jquery_ujs
 //= require twitter/bootstrap
 //= require chosen-jquery
 //= require select2
