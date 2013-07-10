c73f73c6158be10bef084b961d58776abdd51a5a
lmhsjackson <josepiccioni@gmail.com>
2013-07-10 14:25:28 -0300

    So far so good with the refactor, use SEND to evaluate the variable as a method name. The aproach of using [attribute] (with brackets) don't work because some methods are not also atributes.

--- app/reports/expedientes_report.rb
+++ app/reports/expedientes_report.rb
@@ -1,48 +1,42 @@
 class ExpedientesReport
-  def listado(expedientes)
+  EXPEDIENTE_ATTRIBUTES = [ :clave, :autor, :entrada,  :firmantes, :descripcion, :estado, :tema ]
+  EXPEDIENTE_DETAILED_ATTRIBUTES =  [:clave, :autor, :entrada, :tema, :estado, :descripcion, :firmantes, :tipoentr, :hora, :tipoperiod, :numperiodo, :tratamiento, :resultado, :fechases, :periodo]
 
+  def listado(expedientes)
     report = ODFReport::Report.new(Rails.root.join("app/reports/expedientes.odt")) do |r|
       r.add_section "EXPEDIENTES", expedientes do |s|
-        s.add_field(:clave) { |item| item.clave.to_s }
-        s.add_field(:entrada) { |item| item.entrada.to_s }
-        s.add_field(:autor) { |item| item.autor.to_s }
-        s.add_field(:firmantes) { |item| item.firmantes.to_s }
-        s.add_field(:descripcion) { |item| item.descrip.to_s }
-        s.add_field(:estado) { |item| item.estado.to_s }
-        s.add_field(:tema) { |item| item.tema.to_s }
-
+        EXPEDIENTE_ATTRIBUTES.each do |attribute|
+          s.add_field( attribute) { |item| item.send(attribute).to_s }
+        end
       end
     end
-
-    report_file_name = report.generate
-
+    report.generate
   end
 
-  def detalle(params)
-
-    @expediente = Expediente.find(params[:id])
+  def detalle(expediente)
 
     report = ODFReport::Report.new(Rails.root.join("app/reports/expediente.odt")) do |r|
 
-      r.add_field(:clave,         @expediente.clave.to_s)
-      r.add_field(:autor,         @expediente.autor.to_s)
-      r.add_field(:tema,          @expediente.tema.to_s)
-      r.add_field(:estado,        @expediente.estado.to_s)
-      r.add_field(:descripcion,   @expediente.descrip.mb_chars.capitalize)
-      r.add_field(:firmantes,     @expediente.firmantes.to_s)
 
-      r.add_field(:fechaentr,     @expediente.fechaentr.to_s)
-      r.add_field(:tipoentr,      @expediente.tipoentr.to_s)
-      r.add_field(:hora,          @expediente.hora.to_s)
-      r.add_field(:tipoperiod,    @expediente.tipoperiod.to_s)
-      r.add_field(:numperiodo,    @expediente.numperiodo.to_s)
+      r.add_field(:clave,         expediente.clave.to_s)
+      r.add_field(:autor,         expediente.autor.to_s)
+      r.add_field(:tema,          expediente.tema.to_s)
+      r.add_field(:estado,        expediente.estado.to_s)
+      r.add_field(:descripcion,   expediente.descrip.mb_chars.capitalize)
+      r.add_field(:firmantes,     expediente.firmantes.to_s)
 
-      r.add_field(:tratamiento,   @expediente.sesion.try(:tratamiento).to_s)
-      r.add_field(:resultado,     @expediente.sesion.try(:resultado).to_s)
-      r.add_field(:fechases,      @expediente.sesion.try(:fechases).to_s)
-      r.add_field(:periodo,       @expediente.sesion.try(:periodo).to_s)
+      r.add_field(:fechaentr,     expediente.fechaentr.to_s)
+      r.add_field(:tipoentr,      expediente.tipoentr.to_s)
+      r.add_field(:hora,          expediente.hora.to_s)
+      r.add_field(:tipoperiod,    expediente.tipoperiod.to_s)
+      r.add_field(:numperiodo,    expediente.numperiodo.to_s)
 
-      r.add_section "COMISION", @expediente.estados do |s|
+      r.add_field(:tratamiento,   expediente.sesion.try(:tratamiento).to_s)
+      r.add_field(:resultado,     expediente.sesion.try(:resultado).to_s)
+      r.add_field(:fechases,      expediente.sesion.try(:fechases).to_s)
+      r.add_field(:periodo,       expediente.sesion.try(:periodo).to_s)
+
+      r.add_section "COMISION", expediente.estados do |s|
         s.add_field(:nombre) { |estado| estado.comision_nombre }
         s.add_field(:entrada) { |estado| estado.fechaent }
         s.add_field(:salida) { |estado| estado.fechasal }
@@ -53,11 +47,9 @@ class ExpedientesReport
           ss.add_field(:dictamen) { |n| n[:dictamen].to_s }
         end
       end
-
     end
-
-    report_file_name = report.generate
-
+  report.generate
   end
 
 end
+
