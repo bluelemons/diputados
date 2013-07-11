bb6408931cfff676bed61cbb3e049c744cfcaa10
lmhsjackson <josepiccioni@gmail.com>
2013-07-11 14:20:41 -0300

    erased last comments

--- app/reports/expedientes_report.rb
+++ app/reports/expedientes_report.rb
@@ -4,6 +4,7 @@ class ExpedientesReport
   EXPEDIENTE_MAY_HAVE = [:tratamiento, :resultado, :fechases, :periodo]
   COMISION = [:comision_nombre, :fechaentr, :fechasal]
   DICTAMEN = [:tipo, :fecha, :dictamen]
+
   def listado(expedientes)
     report = ODFReport::Report.new(Rails.root.join("app/reports/expedientes.odt")) do |r|
       r.add_section "EXPEDIENTES", expedientes do |s|
@@ -30,21 +31,14 @@ class ExpedientesReport
             r.add_field(possible_attribute, '-')
           end
         end
-
       r.add_section "COMISION", expediente.estados do |s|
         COMISION.each do |attribute|
           s.add_field(attribute) { |item| item.send(attribute).to_s }
         end
-        # s.add_field(:comision_nombre) { |estado| estado.comision_nombre }
-        # s.add_field(:fechaent) { |estado| estado.fechaent }
-        # s.add_field(:fechasal) { |estado| estado.fechasal }
         s.add_section("DICTAMEN", :dictamenes) do |ss|
           DICTAMEN.each do |attribute|
             ss.add_field(attribute) { |item| item.send(attribute).to_s }
           end
-          # ss.add_field(:tipo) { |n| n[:tipo].to_s }
-          # ss.add_field(:fecha) { |n| n[:fecha].to_s }
-          # ss.add_field(:dictamen) { |n| n[:dictamen].to_s }
         end
       end
     end
