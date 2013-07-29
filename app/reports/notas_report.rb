15c8acb75206864b894ef823fc0a4ca4f623d122
lmhsjackson <josepiccioni@gmail.com>
2013-07-29 10:33:53 -0300

    new class notas report

---
+++ app/reports/notas_report.rb
@@ -0,0 +1,29 @@
+class NotasReport
+
+# Numero,  Tipo de iniciador,  Iniciador, Area,  Weekly session,
+# Ingreso, Fojas, Descripcion
+
+  def initialize (notas)
+    this.notas = notas
+  end
+
+  def listado()
+
+    report = ODFReport::Report.new(Rails.root.join("app/reports/notas.odt")) do |r|
+      r.add_section "NOTAS", notas do |n|
+        n.add_field(:numero) { |item| item.numero.to_s }
+        n.add_field(:organizacion) { |item| item.organization.to_s }
+        n.add_field(:autor) { |item| item.initiator.to_s }
+        n.add_field(:area) { |item| item.area.to_s }
+        # n.add_field(:descripcion) { |item| item.primer_pase.descripcion.to_s }
+        # n.add_field(:sesion) { |item| item.weekly_session.to_s }
+        # n.add_field(:ingreso) { |item| item.primer_pase.ingreso.to_s }
+
+      end
+    end
+
+    report.generate
+
+  end
+
+end
