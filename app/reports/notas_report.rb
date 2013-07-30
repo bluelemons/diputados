551a3bcd821e817d60a666288f4c36d093b9af45
lmhsjackson <josepiccioni@gmail.com>
2013-07-30 14:04:19 -0300

    Notas report class now has initialize, and working fields are added and mached

--- app/reports/notas_report.rb
+++ app/reports/notas_report.rb
@@ -3,22 +3,23 @@ class NotasReport
 # Numero,  Tipo de iniciador,  Iniciador, Area,  Weekly session,
 # Ingreso, Fojas, Descripcion
 
-  # def initialize (notas)
-  #   @notas = notas
-  # end
-
-  def self.listado (notas)
+  def initialize (notas)
     @notas = notas
+  end
 
+  def listado
     report = ODFReport::Report.new(Rails.root.join("app/reports/notas.odt")) do |r|
-      r.add_section "NOTAS", @notas do |n|
-        n.add_field(:numero) { |item| item.numero.to_s }
-        n.add_field(:organizacion) { |item| item.organization.to_s }
-        n.add_field(:autor) { |item| item.initiator.to_s }
-        n.add_field(:area) { |item| item.area.to_s }
-        # n.add_field(:descripcion) { |item| item.primer_pase.descripcion.to_s }
-        # n.add_field(:sesion) { |item| item.weekly_session.to_s }
-        # n.add_field(:ingreso) { |item| item.primer_pase.ingreso.to_s }
+      r.add_section "NOTAS", @notas do |s|
+        s.add_field(:numero) { |item| item.numero.to_s }
+        s.add_field(:area) { |item| item.area.to_s }
+        s.add_field(:autor) { |item| item.autor.to_s }
+
+        s.add_field(:organizacion) do |item|
+          # binding.pry
+          item.organization.to_s end
+        # s.add_field(:descripcion) { |item| item.primer_pase.descripcion.to_s }
+        # s.add_field(:sesion) { |item| item.weekly_session.to_s }
+        # s.add_field(:ingreso) { |item| item.primer_pase.ingreso.to_s }
       end
     end
 
